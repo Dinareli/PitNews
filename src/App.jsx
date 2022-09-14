730d1da3078ec0b9b8072935ab5dba0ff7b83c0a
Natalia Dinareli De Lima <natalia.lima@conductor.com.br>
2022-09-14 20:00:18 -0300

    Comentando alguns imports para impedimento de erros até o momento desta alteração

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import MenuLateral from './componentes/MenuLateral';
 import Curtida from './componentes/Curtida';
 import Curtidas from './componentes/Curtidas';
 import Comentarios from './componentes/Comentarios';
-// import CurtirComentar from './componentes/CurtirComentar';
-// import Cabecalho from './componentes/Cabecalho';
+/* import CurtirComentar from './componentes/CurtirComentar'; **/
+/* import Cabecalho from './componentes/Cabecalho'; **/
 import Cadastro from './componentes/Cadastro';
 import Post from './componentes/Post';
 
